c8a9a6c5df4f8e67d5349f30a7ab2382ae3eeb94
kevin-nel <kevinnel4@gmail.com>
2018-12-27 22:24:33 +0200

    Update xmas.py

--- xmas.py
+++ xmas.py
@@ -2,19 +2,17 @@ import termcolor
 import random
 import time
 import datetime
-import platform
-from os import system, name 
+#import platform
+import os
 from colorama import init
 from termcolor import colored
 
 def clear():   
-    name = platform.system()
-    # check if  windows 
-    if name == 'nt': 
-        system('cls') 
-    # if *nix 
-    else: 
-        system('clear') 
+    #name = platform.system()
+    if os.name == 'posix': # if *nix 
+        return os.system('clear')
+    else: #if  windows 
+        return os.system('cls') 
 
 colors = [
         'red',
@@ -34,9 +32,9 @@ lightlist = [yellowlight, cyanlight, magentalight]
 init()
 while True: #exit with ctrl+C
     print('to exit use a keyboard interrupt (ctrl+c)')
-    for i in range(1,30,2):
+    for i in range(1,30,2): #tree
         tree = ''
-        for j in range(i):
+        for j in range(i): #make lights
             randNum=random.randint(0,500)
             if (randNum <= 750) and (randNum >=250) :
                 tree += lightlist[random.randint(0,2)]
@@ -45,11 +43,13 @@ while True: #exit with ctrl+C
         string = '_'*(15-int(i/2))+tree+'_'*(15-int(i/2))+'\n'
         print(string,end='')
     trunk=colored('mWm', 'yellow')
-    for k in range(3):
-        outbuffer2 = '_'*14+trunk+'_'*14+'\n'
-        print(outbuffer2, end='')
-        merry_Christmas =termcolor.colored('Merry Christmas', colors[random.randint(0,len(colors)-1)])
-        outbuffer3 = '_'*8+merry_Christmas+'_'*8+'\n'
-    print(outbuffer3, end='')
-    time.sleep(0.4)
+    for k in range(3): #trunk
+        outbuffer = '_'*14+trunk+'_'*14+'\n'
+        print(outbuffer, end='')
+        merry_Christmas =termcolor.colored('Merry Christmas', colors[random.randint(0,len(colors)-1)]) 
+    outbuffer2 = '_'*8+merry_Christmas+'_'*8+'\n'
+    print(outbuffer2, end='')
+    time.sleep(0.5)
     clear()
+    
+    
